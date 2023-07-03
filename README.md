104cda42cffb1012a3ff0715f2159197a97d33bf
SJ.Lee <eagleduk@gmail.com>
2023-07-03 10:44:59 +0900

    backup 용 env template 작성

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Homestagram
+
+1. 비밀번호 변경
+2. popup 크기 조절
+3. menu 아이콘
+4. popup video 크기 조절
+5. popup video 재생 ( 자동재생, loop )
+6. 색상 스타일
--- src/js/index.ts
+++ src/js/index.ts
@@ -5,54 +5,70 @@ import {getFolders,getFiles} from "./data";
 import { selectTextList, folderPatten, filePatten, fileImagePatten } from "./common";
 
 const main = document.querySelector("main");
-const content = main.querySelector("div#contents");
-const ul = main.querySelector("nav ul");
+const header = document.querySelector("header");
+const article = document.querySelector("article#popup");
 
-(async() => {
-    const domString = await getFolders();
-    const domDocument =  new DOMParser().parseFromString(domString, "text/html");
-    const lists = selectTextList(domDocument);
+const popup = article.querySelector("div.popup--screen");
+const popupCloseBtn = article.querySelector("input.menu--close");
 
-    lists.forEach((month) => {
-        const match = month.match(folderPatten);
-        if(match) {
-            const section = document.createElement("section");
-            
-            const titleDiv = document.createElement("div");
-            titleDiv.className = "contents--title"
-            titleDiv.innerText = month;
-            titleDiv.id = month;
+const contents = main.querySelector("div#contents");
+const nav = main.querySelector("nav");
+
+const ul = nav.querySelector("ul");
+const menuCloseBtn = nav.querySelector("input.menu--close");
+
+const section = contents.querySelector("section");
+
+const openMenu = header.querySelector(".open--menu");
+const titleDiv = header.querySelector(".contents--title");
+
+const contentDiv = section.querySelector(".contents--body");
+
+const handleToggleMenu = (isOpen:boolean) => {
+    if(isOpen) {
+        nav.classList.remove("openMenu");
+    } else {
+        nav.classList.add("openMenu");
+    }
+}
+
+const handleContentSelect = (event: Event, url: string, file: string) => {
+    article.classList.add("openPopup");
+    popup.innerHTML = "";
+    const isImage = file.match(fileImagePatten);
+    if(isImage) {
+        const img = document.createElement("img");
+        img.src=`Gyum's/${url}/${file}`;
+        img.loading = "lazy";
+        img.decoding = "async";
+
+        popup.appendChild(img);
+    } else {
+        const video =document.createElement("video");
+
+        const source = document.createElement("source");
+        source.src = `Gyum's/${url}/${file}`;
+        source.type = "video/mp4";
+
+        video.appendChild(source);
+
+        popup.appendChild(video);
+    }
     
-            const contentDiv = document.createElement("div");
-            contentDiv.className = "contents--body";
-            
-            section.appendChild(titleDiv);
-            section.appendChild(contentDiv);
-            section.dataset.url = month;
+}
 
-            content.appendChild(section);
-    
-            const anchor = document.createElement("a");
-            anchor.href = `#${month}`;
-            anchor.innerText = month;
+const handleNavigation = (event:Event) => {
+    handleToggleMenu(true);
+    if(event.target instanceof HTMLAnchorElement) {
+        const {target: {dataset: {url}}} = event;
 
-            const li = document.createElement("li");
-            li.appendChild(anchor);
-           
-    
-            ul.appendChild(li);
-        }
-    });
-
-    const sections = content.querySelectorAll("section");
-    Array.from(sections).map(section => {
-        const url = section.dataset.url;
-        console.log(url);
-        const content = section.querySelector("div.contents--body");
         (async() => {
+
             const files = await getFiles(url);
             const domDocument =  new DOMParser().parseFromString(files, "text/html");
             const fileLists = selectTextList(domDocument);
+            titleDiv.innerHTML = url;
+            contentDiv.innerHTML = "";
             fileLists.forEach(file => {
                 const match = file.match(filePatten);
                 if(match) {
@@ -63,6 +79,8 @@ const ul = main.querySelector("nav ul");
                     if(isImage) {
                         const img = document.createElement("img");
                         img.src=`Gyum's/${url}/${file}`;
+                        img.loading = "lazy";
+                        img.decoding = "async";
     
                         div.appendChild(img);
                     } else {
@@ -77,30 +95,55 @@ const ul = main.querySelector("nav ul");
                         div.appendChild(video);
                     }
 
-                    content.appendChild(div);
+                    div.addEventListener("click", (event) => handleContentSelect(event, url, file));
+                    contentDiv.appendChild(div);
                 }
             })
         })();
+    }
+}
 
+(async() => {
+    const domString = await getFolders();
+    const domDocument =  new DOMParser().parseFromString(domString, "text/html");
+    const lists = selectTextList(domDocument);
+
+    let loaded = false;
+
+    lists.forEach((month, index) => {
+        const match = month.match(folderPatten);
+        if(match) {    
+            const anchor = document.createElement("a");
+            anchor.href = `#${month}`;
+            anchor.innerText = month;
+            anchor.dataset.url = month;
+            anchor.addEventListener("click", handleNavigation)
+
+            const li = document.createElement("li");
+            li.appendChild(anchor);           
+    
+            ul.appendChild(li);
+
+            if(!loaded) {
+                anchor.click();
+                loaded = true;
+            }
+        }
     });
 })();
 
-console.log("ff", );
+openMenu.addEventListener("click", () => {
+    handleToggleMenu(nav.classList.contains("openMenu"));
+})
 
-if(false) {
-    
+menuCloseBtn.addEventListener("click", () => {
+    handleToggleMenu(true);
+})
 
-console.log("main", main);
+nav.addEventListener("click", () => {
+    handleToggleMenu(true);
+})
 
-console.log("content", content);
-
-console.log("ul", ul);
-
-console.log("data", getFolders());
-const xml =  new DOMParser().parseFromString("", "text/html");
-console.log("xml", xml);
-const lists = selectTextList(xml);
-console.log("lists", lists);
-
-
-}
+popupCloseBtn.addEventListener("click", () => {
+    article.classList.remove("openPopup");
+})
